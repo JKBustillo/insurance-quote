01db26f0f5c11eafa52892a97aaf3434d61e3437
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-03 22:56:07 -0500

    Form added and styled components in Header and Form added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+import Form from './components/Form';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorForm = styled.div`
+  background-color: #FFFFFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header titulo="Cotizador de seguros" />
+    <Contenedor>
+      <Header titulo="Cotizador de seguros" />
+
+      <ContenedorForm>
+        <Form />
+      </ContenedorForm>
+    </Contenedor>
   );
 }
 
